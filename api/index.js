1023f7acdddba6268e8accf819f8e8944211cc2e
Thomas <tom@Thomass-MacBook-Air.local>
2024-08-12 20:33:02 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
-import express from "express";
+import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
-const app = express()
 
-app.listen(3000, ()=>{
-    console.log("Server is running")
+
+mongoose.connect('mongodb://localhost:27017/mongodb-mern').then(()=>{console.log("database connect")}).catch((err)=>{console.log("database error")})
+
+const app = express();
+
+app.listen(3000, () =>{
+    console.log("Server running ")
 })
